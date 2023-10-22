86ec1460cd24a1b37a829754cca24ec7ecf52ab5
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2023-10-22 22:06:46 +0200

    Update preview_blogentry
    I have reached perfection!

--- preview_blogentry.sh
+++ preview_blogentry.sh
@@ -9,36 +9,6 @@ cd ~/repos/lazyblorg
 rm -rf testdata/2del/*
 mkdir testdata/2del/blog
 
-## This script provides a quick preview of the current blog entry in your local browser.
-## You will need a LISP function similar to the one below:
-##
-##    (defun my-lazyblorg-test()
-## 	"Saves current blog entry to file and invoke lazyblorg process with it"
-## 	(interactive)
-## 	(save-excursion
-## 	  (search-backward ":blog:");; search begin of current (previous) blog entry
-## 	  (beginning-of-line nil)
-## 	  (set-mark-command nil);; set mark
-## 	  (org-cycle);; close org-mode heading and sub-headings
-## 	  (next-line);; goto next org-mode heading (this should be next line after blog entry)
-## 	  (beginning-of-line nil)
-## 	  (let ((p (point));; copy region
-## 		(m (mark)))
-## 	    (if (< p m)
-## 		(kill-ring-save p m)
-## 	      (kill-ring-save m p)))
-## 	  (find-file "/tmp/lazyblorg-preview.org");; hard coded temporary file (will be overwritten)
-## 	  (erase-buffer);; I told you!
-## 	  (yank);; paste region from above
-## 	  (save-buffer);; save to disk
-## 	  (kill-buffer "lazyblorg-preview.org");; destroy last evidence
-## 	  (previous-line);;
-## 	  (org-cycle);; close org-mode heading and sub-headings
-## 	  ;; invoke lazyblorg:
-## 	  (shell-command-to-string "/home/vk/src/lazyblorg/preview_blogentry.sh");; invoke shell script
-## 	  )
-## 	)
-
 PYTHONPATH="$HOME/repos/lazyblorg"
 
 "$PYTHONPATH/lazyblorg.py" --quiet \
@@ -47,10 +17,19 @@ PYTHONPATH="$HOME/repos/lazyblorg"
     --previous-metadata NONEXISTING-preview-metadata-REPLACE_WITH_YOUR_PREVIOUS_METADATA_FILE.pk \
     --new-metadata testdata/2del/blog/preview-metadata.pk \
     --logfile testdata/2del/errors.org \
-    --orgfiles testdata/end_to_end_test/orgfiles/about-placeholder.org \
-               templates/blog-format.org \
-               /tmp/lazyblorg-preview.org \
-    && find testdata/2del -name '*.html' -exec xdg-open "{}" \;
+    --orgfiles templates/blog-format.org\
+ testdata/end_to_end_test/orgfiles/about-placeholder.org \
+ /tmp/lazyblorg-preview.org;
+# xdg-open "/home/tassilo/repos/lazyblorg/testdata/2del/blog/index.html"
+# output=$(find testdata/2del/ -name '*.html' -exec xdg-open "{}" \; 2>&1)
+# echo "$output" > /tmp/xdg-open.log
+find testdata/2del -name '*.html' \
+  -not -path 'testdata/2del/blog/tags/*' \
+  -a -not -path 'testdata/2del/blog/about/*' \
+  -a -not -path 'testdata/2del/blog/how-to-use-this-blog/*' \
+  -a -not -path 'testdata/2del/blog/2016/*' \
+  -a -not -wholename 'testdata/2del/blog/index.html' \
+  -exec firefox "{}" \;
     #I'm not sure what the \; is for, but it makes -exec work don't mess with it future me.
 
 #end
